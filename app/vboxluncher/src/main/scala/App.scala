3672ce9ec639a255e35a6ae1383388e390ff5613
numa08 <n511287@gmail.com>
2013-06-08 14:39:29 +0900

    サブコマンドできた

--- app/vboxluncher/src/main/scala/App.scala
+++ app/vboxluncher/src/main/scala/App.scala
@@ -4,7 +4,26 @@ object App {
 
   def main(args: Array[String]) {
   	args.toList match {
-  		case "status" :: subArgs=> println("status command")
+  		case "status" :: subArgs=> subArgs match {
+  			case "all" :: Nil => println("status all")
+  			case "name" :: names => println("show boxes satatus")
+  			case _ => println(usage)
+  		}
+
+  		case "start" :: subArgs => subArgs match {
+  			case "all" :: Nil => println("start all")
+  			case "name" :: names => println("start boxes")
+  			case _ => println(usage)
+  		}
+
+  		case "stop" :: subArgs => subArgs match {
+  			case "all" :: Nil => println("stop all")
+  			case "name" :: names => println("stop boxes")
+  			case _ => println(usage)
+  		}
+  		
+  		
+  		
   		case _ => println(usage)
   	}
   	
