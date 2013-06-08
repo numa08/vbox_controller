3f5bbf067086627a8dddbb4cfacc948d7c9d8924
numa08 <n511287@gmail.com>
2013-06-08 14:23:02 +0900

    使い方がでるようになった

--- app/vboxluncher/src/main/scala/App.scala
+++ app/vboxluncher/src/main/scala/App.scala
@@ -1,7 +1,25 @@
 package net.numa08.vboxluncher
 
 object App {
+
   def main(args: Array[String]) {
-    print("Hello net.numa08.VboxLuncher!")
+  	args.toList match {
+  		case "status" :: subArgs=> println("status command")
+  		case _ => println(usage)
+  	}
+  	
   }
+
+  def run(args: Array[String]):Int = {
+  	try { 
+  		main(args)
+  		0
+  	} catch {
+  	  case e: Exception => 1
+  	}
+  }
+
+  val usage = """|usage:
+  				 | this is usage
+  			  """.stripMargin
 }
